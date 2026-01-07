094e953576f45d4678c78608fe0ecad94ad3eb38
amitsaviro <saviramit@gmail.com>
2026-01-07 12:19:40 +0000

    "ConnectionsImpl"

--- server/src/main/java/bgu/spl/net/srv/Connections.java
+++ server/src/main/java/bgu/spl/net/srv/Connections.java
@@ -4,9 +4,9 @@ import java.io.IOException;
 
 public interface Connections<T> {
 
-    boolean send(int connectionId, T msg);
+    boolean send(int connectionId, T msg); //YA sending msg to client with connectionId
 
-    void send(String channel, T msg);
+    void send(String channel, T msg);// YA sending msg to all clients subscribed to channel
 
-    void disconnect(int connectionId);
+    void disconnect(int connectionId);// YA disconnect client with connectionId
 }
